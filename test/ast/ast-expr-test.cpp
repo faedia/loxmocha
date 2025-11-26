67349cbc1e0e2ffa4e8e051d4eddc45a70e8f975
Emma Tomlinson <t97emma@gmail.com>
2025-11-26 21:37:39 +0000

    Add helper to make is easier to test ASTs by walking actual and expected together

--- test/ast/ast-expr-test.cpp
+++ test/ast/ast-expr-test.cpp
@@ -8,6 +8,7 @@
 #include <cstddef>
 #include <print>
 #include <string>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -167,4 +168,190 @@ TEST(ExprTest, ParserCallTest)
     for (const auto& diag : result.diagnostics()) {
         std::print("Diagnostic: {}", diag);
     }
+}
+
+class expr_assert_visitor {
+public:
+    void operator()([[maybe_unused]] const auto& actual, [[maybe_unused]] const auto& expected)
+    {
+        FAIL() << "Unexpected expression type encountered in assertion visitor.";
+    }
+
+    void operator()(const auto& actual, const loxmocha::expr::expr_t& expected)
+    {
+        ASSERT_TRUE(expected.is<std::decay_t<decltype(actual)>>());
+        this->operator()(actual, expected.as<std::decay_t<decltype(actual)>>());
+    }
+
+    void operator()(const loxmocha::expr::literal_t& actual, const loxmocha::expr::literal_t& expected)
+    {
+        ASSERT_TRUE(actual.value().kind() == loxmocha::token_t::kind_e::l_integer
+                    || actual.value().kind() == loxmocha::token_t::kind_e::l_char
+                    || actual.value().kind() == loxmocha::token_t::kind_e::l_string);
+
+        ASSERT_EQ(actual.value().kind(), expected.value().kind());
+        ASSERT_EQ(actual.value().span(), expected.value().span());
+    }
+
+    void operator()(const loxmocha::expr::identifier_t& actual, const loxmocha::expr::identifier_t& expected)
+    {
+        ASSERT_EQ(actual.name().kind(), expected.name().kind());
+        ASSERT_EQ(actual.name().kind(), expected.name().kind());
+        ASSERT_EQ(actual.name().span(), expected.name().span());
+    }
+
+    void operator()(const loxmocha::expr::binary_t& actual, const loxmocha::expr::binary_t& expected)
+    {
+        ASSERT_EQ(actual.op().kind(), expected.op().kind());
+        actual.left()->visit(*this, *expected.left());
+        actual.right()->visit(*this, *expected.right());
+    }
+
+    void operator()(const loxmocha::expr::unary_t& actual, const loxmocha::expr::unary_t& expected)
+    {
+        ASSERT_EQ(actual.op().kind(), expected.op().kind());
+        actual.operand()->visit(*this, *expected.operand());
+    }
+
+    void operator()(const loxmocha::expr::array_t& actual, const loxmocha::expr::array_t& expected)
+    {
+        ASSERT_EQ(actual.elements().size(), expected.elements().size());
+        for (size_t i = 0; i < actual.elements().size(); ++i) {
+            actual.elements()[i].visit(*this, expected.elements()[i]);
+        }
+    }
+
+    void operator()(const loxmocha::expr::is_t& actual, const loxmocha::expr::is_t& expected)
+    {
+        actual.expr()->visit(*this, *expected.expr());
+        actual.type()->visit(*this, *expected.type());
+    }
+
+    void operator()(const loxmocha::expr::cast_t& actual, const loxmocha::expr::cast_t& expected)
+    {
+        actual.expr()->visit(*this, *expected.expr());
+        actual.type()->visit(*this, *expected.type());
+    }
+
+    void operator()(const loxmocha::expr::tuple_t& actual, const loxmocha::expr::tuple_t& expected)
+    {
+        ASSERT_EQ(actual.elements().size(), expected.elements().size());
+        for (size_t i = 0; i < actual.elements().size(); ++i) {
+            actual.elements()[i].visit(*this, expected.elements()[i]);
+        }
+    }
+
+    void operator()(const loxmocha::expr::record_t& actual, const loxmocha::expr::record_t& expected)
+    {
+        ASSERT_EQ(actual.fields().size(), expected.fields().size());
+        for (size_t i = 0; i < actual.fields().size(); ++i) {
+            ASSERT_EQ(actual.fields()[i].name.kind(), loxmocha::token_t::kind_e::k_identifier);
+            ASSERT_EQ(actual.fields()[i].name.kind(), expected.fields()[i].name.kind());
+            ASSERT_EQ(actual.fields()[i].name.span(), expected.fields()[i].name.span());
+            actual.fields()[i].value.visit(*this, expected.fields()[i].value);
+        }
+    }
+
+    void operator()(const loxmocha::expr::index_t& actual, const loxmocha::expr::index_t& expected)
+    {
+        actual.base()->visit(*this, *expected.base());
+        actual.index()->visit(*this, *expected.index());
+    }
+
+    void operator()(const loxmocha::expr::field_t& actual, const loxmocha::expr::field_t& expected)
+    {
+        ASSERT_EQ(actual.field_name().kind(), loxmocha::token_t::kind_e::k_identifier);
+        ASSERT_EQ(actual.field_name().kind(), expected.field_name().kind());
+        ASSERT_EQ(actual.field_name().span(), expected.field_name().span());
+        actual.base()->visit(*this, *expected.base());
+    }
+
+    void operator()(const loxmocha::expr::call_t& actual, const loxmocha::expr::call_t& expected)
+    {
+        actual.callee()->visit(*this, expected.callee());
+
+        ASSERT_EQ(actual.positional_args().size(), expected.positional_args().size());
+        for (size_t i = 0; i < actual.positional_args().size(); ++i) {
+            actual.positional_args()[i].visit(*this, expected.positional_args()[i]);
+        }
+
+        ASSERT_EQ(actual.named_args().size(), expected.named_args().size());
+        for (size_t i = 0; i < actual.named_args().size(); ++i) {
+            ASSERT_EQ(actual.named_args()[i].name.kind(), loxmocha::token_t::kind_e::k_identifier);
+            ASSERT_EQ(actual.named_args()[i].name.kind(), expected.named_args()[i].name.kind());
+            ASSERT_EQ(actual.named_args()[i].name.span(), expected.named_args()[i].name.span());
+            actual.named_args()[i].value.visit(*this, expected.named_args()[i].value);
+        }
+    }
+
+    void operator()(const loxmocha::expr::if_t& actual, const loxmocha::expr::if_t& expected)
+    {
+        actual.condition()->visit(*this, *expected.condition());
+        actual.then_branch()->visit(*this, *expected.then_branch());
+        if (actual.else_branch() && expected.else_branch()) {
+            actual.else_branch()->visit(*this, *expected.else_branch());
+        } else {
+            ASSERT_EQ(!!actual.else_branch(), !!expected.else_branch());
+        }
+    }
+
+    void operator()(const loxmocha::expr::while_t& actual, const loxmocha::expr::while_t& expected)
+    {
+        actual.condition()->visit(*this, *expected.condition());
+        actual.body()->visit(*this, *expected.body());
+    }
+
+    void operator()(const loxmocha::expr::block_t& actual, const loxmocha::expr::block_t& expected)
+    {
+        ASSERT_EQ(actual.statements().size(), expected.statements().size());
+        for (size_t i = 0; i < actual.statements().size(); ++i) {
+            actual.statements()[i].visit(*this, expected.statements()[i]);
+        }
+    }
+
+    void operator()(const loxmocha::expr::grouping_t& actual, const loxmocha::expr::grouping_t& expected)
+    {
+        actual.expression()->visit(*this, *expected.expression());
+    }
+};
+
+namespace {
+auto make_expr(loxmocha::expr::expr_t&& expr) -> loxmocha::safe_ptr<loxmocha::expr::expr_t>
+{
+    return loxmocha::safe_ptr<loxmocha::expr::expr_t>::make(std::move(expr));
+}
+} // namespace
+
+TEST(ExprTest, ParserBinaryExprPrecedenceTest)
+{
+    using namespace loxmocha;
+
+    const auto*       parse_string = "a + b * c - d / e";
+    loxmocha::lexer_t lexer{parse_string};
+    auto              result = loxmocha::parse_expr(lexer);
+
+    ASSERT_TRUE(result);
+
+    // We expect the following AST:
+    // binary p_minus
+    //     binary p_plus
+    //         identifier a
+    //         binary p_asterisk
+    //             identifier b
+    //             identifier c
+    //     binary p_slash
+    //         identifier d
+    //         identifier e
+    result.result().visit(
+        expr_assert_visitor{},
+        expr::binary_t{token_t::p_minus("-"),
+                       make_expr(expr::binary_t{
+                           token_t::p_plus("+"),
+                           make_expr(expr::identifier_t{token_t::k_identifier("a")}),
+                           make_expr(expr::binary_t{token_t::p_asterisk("*"),
+                                                    make_expr(expr::identifier_t{token_t::k_identifier("b")}),
+                                                    make_expr(expr::identifier_t{token_t::k_identifier("c")})})}),
+                       make_expr(expr::binary_t{token_t::p_slash("/"),
+                                                make_expr(expr::identifier_t{token_t::k_identifier("d")}),
+                                                make_expr(expr::identifier_t{token_t::k_identifier("e")})})});
 }
