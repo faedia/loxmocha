02027025daa21972db931ecef877e61be83bc5a3
Emma Tomlinson <t97emma@gmail.com>
2025-12-13 01:37:41 +0000

    Add source file management.
    We can register source spans associated with a file path.
    We can then find source files that contain a span.
    Given a source_view retrieved from the source manager we can get the
    line and column number of a span within the source view

---
+++ src/source/include/loxmocha/source/source.hpp
@@ -0,0 +1,121 @@
+#pragma once
+
+#include "loxmocha/memory/safe_pointer.hpp"
+
+#include <cstddef>
+#include <filesystem>
+#include <map>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+namespace loxmocha::source {
+
+struct source_location_t {
+    std::string_view line_span;
+    std::size_t      line;
+    std::size_t      column;
+};
+
+struct span_less_t {
+    auto operator()(const std::string_view& lhs, const std::string_view& rhs) const -> bool
+    {
+        return lhs.data() < rhs.data();
+    }
+};
+
+class source_info_t {
+public:
+    source_info_t(std::filesystem::path filepath, std::string&& content);
+    source_info_t() = delete;
+
+    [[nodiscard]] auto filepath() const -> const std::filesystem::path& { return filepath_; }
+    [[nodiscard]] auto content() const -> std::string_view { return content_; }
+    [[nodiscard]] auto lines() const -> std::span<const std::string_view> { return lines_; }
+    [[nodiscard]] auto find_location(std::string_view source_span) const -> std::optional<source_location_t>;
+
+private:
+    std::filesystem::path         filepath_;
+    std::string                   content_;
+    std::vector<std::string_view> lines_;
+};
+
+class source_view_t {
+public:
+    source_view_t(std::filesystem::path filepath, std::string_view content, std::span<const std::string_view> lines)
+        : filepath_(std::move(filepath)), content_(content), lines_(lines)
+    {
+    }
+
+    [[nodiscard]] auto filepath() const -> const std::filesystem::path& { return filepath_; }
+    [[nodiscard]] auto content() const -> std::string_view { return content_; }
+    [[nodiscard]] auto lines() const -> std::span<const std::string_view> { return lines_; }
+    [[nodiscard]] auto find_location(std::string_view source_span) const -> std::optional<source_location_t>;
+
+private:
+    std::filesystem::path             filepath_;
+    std::string_view                  content_;
+    std::span<const std::string_view> lines_;
+};
+
+class source_manager_t {
+public:
+    class iterator_t {
+    public:
+        auto operator++() -> iterator_t&
+        {
+            ++internal_iterator_;
+            return *this;
+        }
+
+        [[nodiscard]] auto operator*() const -> source_view_t { return this->view(); }
+        [[nodiscard]] auto operator->() const -> source_view_t { return this->view(); }
+        [[nodiscard]] auto operator==(const iterator_t& other) const -> bool
+        {
+            return internal_iterator_ == other.internal_iterator_;
+        }
+
+        [[nodiscard]] auto operator!=(const iterator_t& other) const -> bool { return !(*this == other); }
+
+        [[nodiscard]] auto view() const -> source_view_t
+        {
+            return source_view_t{internal_iterator_->second->filepath(),
+                                 internal_iterator_->second->content(),
+                                 internal_iterator_->second->lines()};
+        }
+
+    private:
+        friend class source_manager_t;
+        explicit iterator_t(std::unordered_map<std::filesystem::path, safe_ptr<source_info_t>>::const_iterator iter)
+            : internal_iterator_(iter)
+        {
+        }
+        std::unordered_map<std::filesystem::path, safe_ptr<source_info_t>>::const_iterator internal_iterator_;
+    };
+
+    source_manager_t() = default;
+
+    source_manager_t(const source_manager_t&)     = delete;
+    source_manager_t(source_manager_t&&) noexcept = default;
+    ~source_manager_t()                           = default;
+
+    auto operator=(const source_manager_t&) -> source_manager_t&     = delete;
+    auto operator=(source_manager_t&&) noexcept -> source_manager_t& = default;
+
+    auto emplace(safe_ptr<source_info_t>&& source_info) -> std::pair<iterator_t, bool>;
+
+    [[nodiscard]] auto find_source(std::string_view source_span) const -> iterator_t;
+    [[nodiscard]] auto find_source(const std::filesystem::path& filepath) const -> iterator_t;
+
+    [[nodiscard]] auto begin() -> iterator_t;
+    [[nodiscard]] auto end() -> iterator_t;
+
+private:
+    std::unordered_map<std::filesystem::path, safe_ptr<source_info_t>> filepath_map_;
+    std::map<std::string_view, source_info_t&, span_less_t>            source_map_;
+};
+
+} // namespace loxmocha::source
---
+++ src/source/source.cpp
@@ -0,0 +1,113 @@
+#include "loxmocha/source/source.hpp"
+
+#include "loxmocha/memory/safe_pointer.hpp"
+
+#include <algorithm>
+#include <cstddef>
+#include <filesystem>
+#include <iterator>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+namespace loxmocha::source {
+
+namespace {
+    auto make_lines(std::string_view content) -> std::vector<std::string_view>
+    {
+        std::vector<std::string_view> lines{};
+        const auto*                   line_start = content.begin();
+        for (const auto* iter = content.begin(); iter != content.end(); std::advance(iter, 1)) {
+            if (*iter == '\n') {
+                lines.emplace_back(line_start, iter);
+                line_start = std::next(iter);
+            }
+        }
+        if (line_start != content.end()) {
+            lines.emplace_back(line_start, content.end());
+        }
+
+        return lines;
+    }
+} // namespace
+
+source_info_t::source_info_t(std::filesystem::path filepath, std::string&& content)
+    : filepath_(std::move(filepath)), content_(std::move(content)), lines_(make_lines(content_))
+{
+}
+
+auto source_info_t::find_location(std::string_view source_span) const -> std::optional<source_location_t>
+{
+    return source_view_t{filepath_, content_, lines_}.find_location(source_span);
+}
+
+auto source_view_t::find_location(std::string_view source_span) const -> std::optional<source_location_t>
+{
+    auto line = std::ranges::lower_bound(lines_.begin(),
+                                         lines_.end(),
+                                         source_span,
+                                         [](const std::string_view& line, const std::string_view& span) -> bool {
+                                             // cppcheck-suppress mismatchingContainerExpression
+                                             return line.end() <= span.begin();
+                                         });
+
+    if (line == lines_.end()) {
+        return std::nullopt;
+    }
+
+    return source_location_t{
+        .line_span = *line,
+        .line      = static_cast<std::size_t>(std::distance(lines_.begin(), line) + 1),
+        .column    = static_cast<std::size_t>(source_span.data() - line->data() + 1),
+    };
+}
+
+auto source_manager_t::emplace(safe_ptr<source_info_t>&& source_info) -> std::pair<iterator_t, bool>
+{
+    auto result = filepath_map_.emplace(source_info->filepath(), std::move(source_info));
+
+    if (result.second) {
+        source_map_.emplace(result.first->second->content(), *(result.first->second));
+    }
+
+    return {iterator_t{result.first}, result.second};
+}
+
+auto source_manager_t::find_source(const std::filesystem::path& filepath) const -> source_manager_t::iterator_t
+{
+    auto iter = filepath_map_.find(filepath);
+    if (iter == filepath_map_.end()) {
+        return source_manager_t::iterator_t{filepath_map_.end()};
+    }
+
+    return source_manager_t::iterator_t{iter};
+}
+
+auto source_manager_t::find_source(std::string_view source_span) const -> source_manager_t::iterator_t
+{
+    auto upper = source_map_.upper_bound(source_span);
+    if (upper == source_map_.begin()) {
+        return source_manager_t::iterator_t{filepath_map_.end()};
+    }
+
+    auto iter = std::prev(upper);
+    if (iter->first.begin() <= source_span.begin() && source_span.end() <= iter->first.end()) {
+        return source_manager_t::iterator_t{filepath_map_.find(iter->second.filepath())};
+    }
+
+    return source_manager_t::iterator_t{filepath_map_.end()};
+}
+
+auto source_manager_t::begin() -> source_manager_t::iterator_t
+{
+    return source_manager_t::iterator_t{filepath_map_.begin()};
+}
+
+auto source_manager_t::end() -> source_manager_t::iterator_t
+{
+    return source_manager_t::iterator_t{filepath_map_.end()};
+}
+
+} // namespace loxmocha::source
